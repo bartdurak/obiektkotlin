4e7cc5c3e6e59c03246419942d4951acfe990df0
Bart Durak <projektator@gmail.com>
2023-11-20 06:05:43 +0100

    03 (typ float)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,4 +12,10 @@ fun main(args: Array<String>)
     println("dla inta: " + UInt.MAX_VALUE)
     println("dla inta wartość , UInt wynosi: " + UInt.MIN_VALUE)
     // ! Dopisz typy danch które znasz.
+    // Do typu zmiennoprzecinkowego dodajemy sufiks F na końcu
+    var float: Float = 3.14F
+    println("float: " + Float.MAX_VALUE)
+    var double: Double = 10.23
+println("double: " + Double.MAX_VALUE)
+
 }
