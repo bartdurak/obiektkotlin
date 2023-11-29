48ad78debace76716788c2aff010d889d9092b7a
budownictwa <budownictwa@zoho.com>
2023-11-29 15:16:56 +0100

    04 (math)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-fun main(args: Array<String>) {
-    if (5 > 3) {
-        println("Prawda")
-    } else {
-        println("Fałsz")
-    }
-}
+fun main(args: Array<String>)
+{
+//typy danych
+    var zmienna: Int= 5
+    // zmienna stała val
+   // val stala = 3.14
+    println(zmienna) println(" plus int : "+ Int.MAX_VALUE)
+    println(" minus int : "+ Int.MIN_VALUE) // dla przykładu sprawdzamy typ danych Byte var byte: Byte println("byte: " + Byte.MAX_VALUE)
+    //var int: UInt
+    println("dla inta: " + UInt.MAX_VALUE)
+    //var int; UInt
+   // println(UInt.MIN_VALUE)
+// ! Dopisz typy danch które znasz. }
