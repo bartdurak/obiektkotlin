9910f43ab3dc3b03fe4a7a86d72a15f04b5782c5
budownictwa <budownictwa@zoho.com>
2023-12-11 14:08:12 +0100

    2.5 (return)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,12 @@
-fun main(args: Array<String>) {
-    run {
-        add(5, 10)
-        //deklaracje zmiennych w tablicy
-   // delkaracja zmiennych w tablicy trzy indeksowej
-    add(20, 100, 40)
-    }
-}
-fun hello(name: String) {
+fun main (args: Array<String>) {
+    var x: Int =add(10, 5)
+    println(x * x)
+    println(add(1,11))
 
-    println("Hello, $name!")
 }
-// tablica dwu indeksowa
-        fun add(a: Int, b: Int) {
-            println("Suma =  ${a + b}")
-        }
-
-// wywoałanie tablicy trzy indeksowej
-fun add (a: Int, b: Int,c: Int) {
-    println("Suma =  ${a + b + c}")
+fun add (a: Int, b: Int): Int {
+ var x = a+b
+ return x
 }
-////
 
 
