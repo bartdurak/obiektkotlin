b6087491b2e1ab00bc3e2214912bcf189c446573
bartdurak <bartedr@hotmail.com>
2023-12-17 23:22:40 +0100

    2.7 (do while)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,19 +2,20 @@ fun main(args: Array<String>)
 // pętla while
 {
     var i = 0
-    var j = 0
+
     while (i < 10) {
+        //! Pętla do while. Wykonuje instrukcje i dopiero sprawdza, czy instrukcja nie zakończy się.
+        // np dla i<100
         //println(" i =   $i")
+    println(" dla zmiennej  : i = $i  ")
         i++
-        // ostania iteracja
-        j = 0
-        // pętla w pętli while
-while (j < 3)
-{
-    println(" dla zmiennych  : i = $i,  j = $j")
-    j++
-}
-
-    }
 
 }
+
+    var j = 0
+    do {
+        println(" dla zmiennej  : j = $j  ")
+        j++
+    } while (j < 10)
+
+        }
