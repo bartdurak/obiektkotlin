51abe2ea1ad08416720887478a6e6feb956ea3bf
bartdurak <bartedr@hotmail.com>
2023-12-17 23:07:51 +0100

    2.6 (while)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,20 @@
 fun main(args: Array<String>)
 // pętla while
 {
-    var i= 0
-    while (i < 10)
-    {
-        println(" i =   $i")
-    i++
+    var i = 0
+    var j = 0
+    while (i < 10) {
+        //println(" i =   $i")
+        i++
+        // ostania iteracja
+        j = 0
+        // pętla w pętli while
+while (j < 3)
+{
+    println(" dla zmiennych  : i = $i,  j = $j")
+    j++
+}
+
     }
+
 }
