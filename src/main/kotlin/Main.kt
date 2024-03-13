da78d58294f5984d3f76cba6bdac198536648e09
budownictwa <budownictwa@zoho.com>
2024-03-13 15:07:49 +0100

    03 (obiekt)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,11 @@
 fun main(args: Array<String>)
-// pętla while
 {
-    var i = 0
 
-    while (i < 10) {
-        //! Pętla do while. Wykonuje instrukcje i dopiero sprawdza, czy instrukcja nie zakończy się.
-        // np dla i<100
-        //println(" i =   $i")
-    println(" dla zmiennej  : i = $i  ")
-        i++
+    heloo("Konefał")
+}
+fun heloo(name: String)
+// string typ danych
+{
+    println("Witaj w $name")
 
 }
-
-    var j = 0
-    do {
-        println(" dla zmiennej  : j = $j  ")
-        j++
-    } while (j < 10)
-
-        }
